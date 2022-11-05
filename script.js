c022d95c5bb0bfe9851bd16baadf5fd2edcd0785
Matouš <matthewproch12@gmail.com>
2022-11-05 21:51:01 +0100

    fixed positioning of button and more

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-let btn = document.querySelector("button")
+var btn = document.querySelector("a")
 let move = true
+let tries = 0;
+const h1 = document.querySelector("h1")
+
 
 function btnOnHover() {
     if (move === true) {
-        btn.style.marginTop = Math.floor(Math.random() * 1000) + "px"  
-        btn.style.marginLeft = Math.floor(Math.random() * 1000) + "px"  
-
+        btn.style.top = Math.floor(Math.random() * 80) + "vh"  
+        btn.style.left = Math.floor(Math.random() * 80) + "vw" 
+        tries += 1
+        console.log(tries);
+        checkCompletition()
+    } else { 
+        btn.innerHTML = "you won"
+        h1.style.display = "inline-block"
         move = false
-    } else {
-        btn.style.marginTop = Math.floor(Math.random() * 1000) + "px"  
-        btn.style.marginLeft = Math.floor(Math.random() * 1000) + "px"  
-        move = true
+        h1.addEventListener("click", function () {move = true; tries = 0; btn.innerHTML = "click me"; h1.style.display = "none";})
     }                      
 
 }
 
+function checkCompletition(params) {
+    if (tries >= 200) {
+        move = false
+    } else {move = true}
+}
+
+
 btn.addEventListener("mouseover", btnOnHover)
+btn.addEventListener("touchstart", btnOnHover)

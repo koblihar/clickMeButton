79f039361e06e6b90b367a969409b9c5c8d26c65
Matouš <matthewproch12@gmail.com>
2022-11-11 12:47:22 +0100

    reworked

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-let btn = document.querySelector("button")
+let btn = document.querySelector("a")
 let move = true
+let num = 1
+let h1 = document.querySelector("h1")
+let yes = document.querySelector(".yes")
+let no = document.querySelector(".no")
 
+//
 function btnOnHover() {
-    if (move === true) {
-        btn.style.marginTop = Math.floor(Math.random() * 1000) + "px"  
-        btn.style.marginLeft = Math.floor(Math.random() * 1000) + "px"  
-
-        move = false
-    } else {
-        btn.style.marginTop = Math.floor(Math.random() * 1000) + "px"  
-        btn.style.marginLeft = Math.floor(Math.random() * 1000) + "px"  
-        move = true
-    }                      
+    if (num != 10 && num < 50) {
+        btn.style.marginTop = Math.floor(Math.random() * 90) + "vh"  
+        btn.style.marginLeft = Math.floor(Math.random() * 90) + "vw"  
+        num += 1
+    } else if (num === 10) {
+        h1.style.display = "inline-block"
+        yes.addEventListener("click", yesf)
+        yes.addEventListener("touchstart", yesf)
+        no.addEventListener("click", nof)
+        yes.addEventListener("touchstart", nof)
+    }
+    else {
+        window.open("http://femboy.in/bambula", '_blank')
+    }                  
 
 }
 
+function yesf() {
+    location.reload()
+            window.open("https://www.youtube.com/watch?v=OgZzUJud3Q4", '_blank')    
+}
+
+function nof() {
+    num += 1
+            h1.style.display = "none"
+}
+
 btn.addEventListener("mouseover", btnOnHover)
+btn.addEventListener("touchstart", btnOnHover)
